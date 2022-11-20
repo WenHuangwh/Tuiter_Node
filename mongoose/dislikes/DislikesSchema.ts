f6417d32f16674343cd9d633d5358f6074372c76
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-11-19 19:13:11 -0500

    add Dislike model type

---
+++ mongoose/dislikes/DislikesSchema.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Define mongoose schema in collection
+ */
+import mongoose, { Schema } from "mongoose";
+import Dislike from "../../models/dislikes/dislike";
+
+const DislikeSchema = new mongoose.Schema<Dislike>({
+    tuit: { type: Schema.Types.ObjectId, ref: "TuitModel" },
+    dislikedBy: { type: Schema.Types.ObjectId, ref: "UserModel" },
+}, { collection: "dislikes" });
+export default DislikeSchema;
